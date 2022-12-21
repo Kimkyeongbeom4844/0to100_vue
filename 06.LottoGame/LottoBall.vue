c59ea53de13ba22b260f3057e2a3c90f047bbed8
kimkyeongbeom <bibix321@naver.com>
2022-12-21 11:37:59 +0900

    로또 코드 리팩토링

--- 06.LottoGame/LottoBall.vue
+++ 06.LottoGame/LottoBall.vue
@@ -36,20 +36,8 @@ export default {
 };
 </script>
 <template>
-  <div class="ball" v-bind:style="{ backgroundColor: setColor }">
+  <div v-bind:style="{ backgroundColor: setColor }">
     {{ number ?? bonusNumber }}
   </div>
 </template>
-<style>
-.ball {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 50px;
-  height: 50px;
-  border: 1px solid black;
-  border-radius: 50%;
-  margin: 0 10px;
-  font-size: 30px;
-}
-</style>
+<style></style>
